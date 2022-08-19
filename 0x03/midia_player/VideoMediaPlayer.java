b2866daa1b4932877c5b61cd7885cbee877023ca
Joao Bohana <jbohana@ciandt.com>
2022-08-19 14:06:25 -0300

    removed println

--- 0x03/midia_player/VideoMediaPlayer.java
+++ 0x03/midia_player/VideoMediaPlayer.java
@@ -2,7 +2,7 @@ public class VideoMediaPlayer implements AdvancedMediaPlayer{
 
     @Override
     public void reproduzirVlc(String nomeArquivo) {
-        System.out.println("Reproduzindo VLC: " + nomeArquivo);
+        System.out.printf("Reproduzindo VLC: " + nomeArquivo);
     }
 
     @Override
