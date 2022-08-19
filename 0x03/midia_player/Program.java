81c426e9a537779d41bbcbf9e587e3669cf9cabc
Joao Bohana <jbohana@ciandt.com>
2022-08-19 14:04:05 -0300

    fix: removed println

--- 0x03/midia_player/Program.java
+++ 0x03/midia_player/Program.java
@@ -8,8 +8,6 @@ public class Program {
         VideoMediaPlayer videoMediaPlayer = new VideoMediaPlayer();
         videoMediaPlayer.reproduzirMp4("Uncharted.mp4");
 
-        System.out.println();
-
         audioPlayer.reproduzir("Moon Knight S01E01.vlc", TipoMedia.VLC);
     }
 }
